59202757ffe5649b1c7aab8068c34ebd01f017d4
Fauzaaannn <fauzanabderrasheed@gmail.com>
2023-11-14 14:52:03 +0700

    futer font

--- project_uas/src/components/futer.vue
+++ project_uas/src/components/futer.vue
@@ -36,21 +36,21 @@
 
         <!-- Section: Links  -->
         <section class="">
-            <div class="container text-center text-md-start mt-5">
+            <div class="container text-center text-md-start mt-4" style="font-family: Product Sans; font-size: 20px;">
                 <!-- Grid row -->
-                <div class="row mt-3">
-                        <!-- Grid column -->
-                        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                            <!-- Content -->
-                            <img src="../assets/Logo Footer.png">
-                        </div>
+                <div class="row">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <!-- Content -->
+                        <img src="../assets/Logo Footer.png">
+                    </div>
                     <!-- Grid column -->
 
                     <!-- Grid column -->
                     <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
                         <!-- Links -->
-                        <h6 class="text-uppercase fw-bold mb-4">
-                            Products
+                        <h6 class="text-uppercase fw-bold mb-4" style="font-family: Product Sans; font-size: 20px;">
+                            Categories
                         </h6>
                         <p>
                             <a href="#!" class="text-reset">Angular</a>
@@ -70,8 +70,8 @@
                     <!-- Grid column -->
                     <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
                         <!-- Links -->
-                        <h6 class="text-uppercase fw-bold mb-4">
-                            Useful links
+                        <h6 class="text-uppercase fw-bold mb-4" style="font-family: Product Sans; font-size: 20px;">
+                            Collection
                         </h6>
                         <p>
                             <a href="#!" class="text-reset">Pricing</a>
@@ -91,7 +91,7 @@
                     <!-- Grid column -->
                     <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                         <!-- Links -->
-                        <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                        <h6 class="text-uppercase fw-bold mb-4" style="font-family: Product Sans; font-size: 20px;">Contact us</h6>
                         <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
                         <p>
                             <i class="fas fa-envelope me-3"></i>
