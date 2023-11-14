9747dff6978218e1c91cad6597e35b3fd9879b70
Fauzaaannn <fauzanabderrasheed@gmail.com>
2023-11-14 14:26:19 +0700

    logo futer

--- project_uas/src/components/futer.vue
+++ project_uas/src/components/futer.vue
@@ -42,13 +42,7 @@
                         <!-- Grid column -->
                         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                             <!-- Content -->
-                            <h6 class="text-uppercase fw-bold mb-4">
-                                <i class="fas fa-gem me-3"></i>Company name
-                            </h6>
-                            <p>
-                                Here you can use rows and columns to organize your footer content. Lorem ipsum
-                                dolor sit amet, consectetur adipisicing elit.
-                            </p>
+                            <img src="../assets/Logo Footer.png">
                         </div>
                     <!-- Grid column -->
 
