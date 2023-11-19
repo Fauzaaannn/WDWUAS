073f4e1a0a4b73b4b85d5737c63e401f42a52b50
Fauzaaannn <fauzanabderrasheed@gmail.com>
2023-11-19 16:07:32 +0700

    script checkbox filter man

--- project_uas/src/components/filterman.vue
+++ project_uas/src/components/filterman.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="card border-0 shadow rounded-4 d-inline-block float-start mb-5" style="width: 18rem; margin-left: 100px;">
-        <div class="card-header custom-category">
+    <div class="card border-0 shadow rounded-4 d-inline-block float-start mb-5 custom-category" style="width: 18rem; margin-left: 100px;">
+        <div class="card-header custom-ctghead">
             Categories
         </div>
         
         <h3 class="ms-4 mt-3">Upper Body</h3>
         <div class="mt-2 ms-5">
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-                <label class="form-check-label" for="flexCheckChecked">
+                <input class="form-check-input" type="checkbox" value="" id="sweater" checked>
+                <label class="form-check-label" for="sweater">
                     Sweater
                 </label>
             </div>
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-                <label class="form-check-label" for="flexCheckChecked">
+                <input class="form-check-input" type="checkbox" value="" id="hoodies" checked>
+                <label class="form-check-label" for="hoodies">
                     Hoodies
                 </label>
             </div>
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-                <label class="form-check-label" for="flexCheckChecked">
+                <input class="form-check-input" type="checkbox" value="" id="shirts" checked>
+                <label class="form-check-label" for="shirts">
                     Shirts 
                 </label>
             </div>
@@ -63,7 +63,7 @@
 
     <div class="d-inline-block ms-5">
     <!-- sweater -->
-        <div id="sweater" class="row">
+        <div id="sweater-div" class="row mb-3">
             <div class="card ms-3 border-0 shadow rounded-4" style="width: 266px;">
                 <img src="../assets/sweater.jpeg" class="card-img-top mt-3 rounded-4">
                 <div class="card-body">
@@ -90,7 +90,7 @@
             </div>
         </div>
     <!-- hoodies -->
-        <div id="hoodies" class="row">
+        <div id="hoodies-div" class="row mb-3">
             <div class="card ms-3 border-0 shadow rounded-4" style="width: 266px;">
                 <img src="../assets/hoodie.jpeg" class="card-img-top mt-3 rounded-4">
                 <div class="card-body">
@@ -117,7 +117,7 @@
             </div>
         </div>
     <!-- shirts -->
-        <div id="shirts" class="row">
+        <div id="shirts-div" class="row mb-3">
             <div class="card ms-3 border-0 shadow rounded-4" style="width: 266px;">
                 <img src="../assets/shirts.jpg" class="card-img-top mt-3 rounded-4">
                 <div class="card-body">
@@ -147,15 +147,68 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Marko+One&family=Poppins:wght@400;600&display=swap');
 
-.custom-category {
-    font-family: Product Sans;
+.custom-ctghead {
+    font-family: Marko One;
     font-size: 45px;
     background-color: white;
+}
+
+.custom-category {
+    font-family: Marko One;
 }
 
 </style>
 
 <script>
+    document.addEventListener('DOMContentLoaded', function () {
+        let sweaterCheckbox = document.getElementById("sweater");
+        let sweaterDiv = document.getElementById("sweater-div");
+
+        sweaterCheckbox.addEventListener('change', function() {
+            if (this.checked) {
+                // Jika checkbox dicentang, tampilkan div dalam satu baris
+                sweaterDiv.style.display = "flex";
+                sweaterDiv.style.flexWrap = "wrap";
+            } else {
+                // Jika checkbox tidak dicentang, sembunyikan div
+                sweaterDiv.style.display = "none";
+            }
+        });
+    });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        // Mendapatkan referensi ke elemen-elemen yang diperlukan
+        let hoodieCheckbox = document.getElementById("hoodies");
+        let hoodieDiv = document.getElementById("hoodies-div");
+
+        // Event listener untuk checkbox hoodies
+        hoodieCheckbox.addEventListener('change', function () {
+            if (this.checked) {
+                // Jika checkbox dicentang, tampilkan div hoodies dalam satu baris
+                hoodieDiv.style.display = "flex";
+                hoodieDiv.style.flexWrap = "wrap";
+            } else {
+                // Jika checkbox tidak dicentang, sembunyikan div hoodies
+                hoodieDiv.style.display = "none";
+            }
+        });
+        
+        let shirtsCheckbox = document.getElementById("shirts");
+        let shirtsDiv = document.getElementById("shirts-div");
+        // Event listener untuk checkbox shirts
+        shirtsCheckbox.addEventListener('change', function () {
+            if (this.checked) {
+                // Jika checkbox dicentang, tampilkan div shirts dalam satu baris
+                shirtsDiv.style.display = "flex";
+                shirtsDiv.style.flexWrap = "wrap";
+            } else {
+                // Jika checkbox tidak dicentang, sembunyikan div shirts
+                shirtsDiv.style.display = "none";
+            }
+        });
+    });
 
 </script>
+
