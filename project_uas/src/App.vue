81d408f8c953d970238516157a7199c638e8b077
Fauzaaannn <fauzanabderrasheed@gmail.com>
2023-11-29 13:56:55 +0700

    router loop fixed

--- project_uas/src/App.vue
+++ project_uas/src/App.vue
@@ -1,11 +1,6 @@
-<script setup>
-import navbar from './components/navbar.vue';
-import collection from './components/collection.vue';
-import preview from './components/preview.vue';
-</script>
-
 <template>
-    <navbar/>
-    <collection/>
-    <preview/>
+<router-view></router-view>
 </template>
+
+<!-- karena component sudah di import di file views, jadi tidak perlu di import disini
+yang perlu ditulis di app.vue hanyalah router-view nya -->
